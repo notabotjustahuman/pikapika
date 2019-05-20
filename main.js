bce02a6b8bdf26e89e3554387672d22189bb6bb4
Billy Chen <billy.chen1206@gmail.com>
2019-05-20 16:32:57 -0400

    pokemon api testing

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-// gengar steelix metagross
+const ALL_POKEMON = []
+pokemon_sorted = { }
+
+
+
+function pokemonFinder() {
+  var pokedex = prompt('Enter Pokémon ID Number')
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadychange = function() {
+    if (this.readyState == 4 && this.status = 200) {
+      let info = JSON.parse(this.responseText)
+      for (item in info) {
+        let pokemon = new Pokemon(item,
+                                  data[item].name,
+                                  date[item].type,)
+        console.log(pokemon)
+      }
+    }
+  }
+  xhttp.open("GET", `http://fizal.me/pokeapi/api/v2/id/${pokedex}.json`), true);
+  xhttp.send();
+}
+
+pokemonFinder()
