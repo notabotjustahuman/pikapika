9730a3043f62363241c245f602995faaf706fcc0
Billy Chen <billy.chen1206@gmail.com>
2019-05-20 16:51:32 -0400

    this api getting me real tight

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 const ALL_POKEMON = []
-pokemon_sorted = { }
-
-
 
 function pokemonFinder() {
   var pokedex = prompt('Enter Pokémon ID Number')
+  console.log(`searched for ${pokedex}`);
   var xhttp = new XMLHttpRequest();
   xhttp.onreadychange = function() {
-    if (this.readyState == 4 && this.status = 200) {
-      let info = JSON.parse(this.responseText)
-      for (item in info) {
+    if (this.readyState == 4 && this.status == 200) {
+      let data = JSON.parse(this.responseText)
+      for (item in data) {
         let pokemon = new Pokemon(item,
-                                  data[item].name,
-                                  date[item].type,)
-        console.log(pokemon)
+                                  data[item].name,)
+                                  // date[item].type,)
+        // console.log(Pokemon)
       }
     }
   }
-  xhttp.open("GET", `http://fizal.me/pokeapi/api/v2/id/${pokedex}.json`), true);
+  xhttp.open("GET", `http://fizal.me/pokeapi/api/v2/id/${pokedex}.json`, true);
   xhttp.send();
+}
+
+class Pokemon {
+  constructor(name) {
+    this.name = name
+    // this.type = type
+    if (ALL_POKEMON.length < 810) {
+      ALL_POKEMON.push(this)
+    }
+  }
 }
 
 pokemonFinder()
