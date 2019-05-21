a1646ba99b1aab80fad8346cb014a86fbc76ffb3
Billy Chen <billy.chen1206@gmail.com>
2019-05-21 16:33:21 -0400

    api actually working

--- main.js
+++ main.js
@@ -1,30 +1,25 @@
-const ALL_POKEMON = []
-
 function pokemonFinder() {
-  var pokedex = prompt('Enter Pokémon ID Number')
+  var pokedex = prompt('Enter Pokémon Name or ID')
   var xhttp = new XMLHttpRequest();
   console.log(`searched for ${pokedex}`)
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       let data = JSON.parse(this.responseText)
-      console.log(data)
-      for (item in data) {
-        let pokemon = new Pokemon(item,
-                                  data[item].name)
-                                  // date[item].type,)
-        ALL_POKEMON.push(pokemon)
+        user = {
+          name: data['name'],
+          hp: data['stats'][5]['base_stat'],
+          attack: data['stats'][4]['base_stat'],
+          defense: data['stats'][3]['base_stat'],
+          speed: data['stats'][0]['base_stat'],
+        }
       }
     }
+  if (isNaN(pokedex)) {
+    xhttp.open("GET", `http://fizal.me/pokeapi/api/v2/name/${pokedex.toLowerCase()}.json`, true);
+  } else {
+    xhttp.open("GET", `http://fizal.me/pokeapi/api/v2/id/${pokedex}.json`, true);
   }
-  xhttp.open("GET", `http://fizal.me/pokeapi/api/v2/id/${pokedex}.json`, true);
   xhttp.send();
 }
 
-class Pokemon {
-  constructor(name) {
-    this.name = name
-    // this.type = type
-  }
-}
-
-pokemonFinder()
+// pokemonFinder()
