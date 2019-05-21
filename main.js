4cfc047f8f3a11420805ea19dc0c81776d433456
Billy Chen <billy.chen1206@gmail.com>
2019-05-21 16:58:48 -0400

    stats gone wild

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ function pokemonFinder() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       let data = JSON.parse(this.responseText)
-        user = {
+        pokemon = {
           name: data['name'],
           hp: data['stats'][5]['base_stat'],
           attack: data['stats'][4]['base_stat'],
           defense: data['stats'][3]['base_stat'],
+          spattack: data['stats'][2]['base_stat'],
+          spdefense: data['stats'][1]['base_stat'],
           speed: data['stats'][0]['base_stat'],
+          abilities: data['abilities'][0]['ability']['name'],
+          abilities2: data['abilities'][1]['ability']['name']
         }
       }
     }
