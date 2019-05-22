9beab2af65a7dccc936c74ed567f77c6b4362de3
Billy Chen <billy.chen1206@gmail.com>
2019-05-22 13:02:48 -0400

    displaying stats on index gone wrong

--- main.js
+++ main.js
@@ -10,11 +10,12 @@ function pokemonFinder() {
           hp: data['stats'][5]['base_stat'],
           attack: data['stats'][4]['base_stat'],
           defense: data['stats'][3]['base_stat'],
+          speed: data['stats'][0]['base_stat'],
           spattack: data['stats'][2]['base_stat'],
           spdefense: data['stats'][1]['base_stat'],
-          speed: data['stats'][0]['base_stat'],
           abilities: data['abilities'][0]['ability']['name'],
-          abilities2: data['abilities'][1]['ability']['name']
+          abilities2: data['abilities'][1]['ability']['name'],
+          sprite: data['sprites']['front_default']
         }
       }
     }
@@ -26,4 +27,42 @@ function pokemonFinder() {
   xhttp.send();
 }
 
-// pokemonFinder()
+pokemonFinder()
+
+displayPokemon = () => {
+  let cont = document.createElement('div')
+  let h1 = document.createElement('h1')
+  let p1 = document.createElement('p1')
+  let p2 = document.createElement('p2')
+  let p3 = document.createElement('p3')
+  let p4 = document.createElement('p4')
+  let p5 = document.createElement('p5')
+  let p6 = document.createElement('p6')
+  let p7 = document.createElement('p7')
+  let p8 = document.createElement('p8')
+  let img = document.createElement('img')
+
+  h1.innerText = pokemon['name']
+  p1.innerText = pokemon['hp']
+  p2.innerText = pokemon['attack']
+  p3.innerText = pokemon['defense']
+  p4.innerText = pokemon['speed']
+  p5.innerText = pokemon['spattack']
+  p6.innerText = pokemon['spdefense']
+  p7.innerText = pokemon['abilities']
+  p8.innerText = pokemon['abilities2']
+  img.setAttribute('src', pokemon['sprite'])
+  cont.appendChild(h1)
+  cont.appendChild(p1)
+  cont.appendChild(p2)
+  cont.appendChild(p3)
+  cont.appendChild(p4)
+  cont.appendChild(p5)
+  cont.appendChild(p6)
+  cont.appendChild(p7)
+  cont.appendChild(p8)
+  cont.appendChild(img)
+  document.body.appendChild(cont)
+}
+
+// displayPokemon()
